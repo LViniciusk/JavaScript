ae04c17fca0d399eb5331a37bf5c19c8c7963858
LViniciusk <botlink2030@gmail.com>
2022-05-18 10:42:13 -0300

    #

---
+++ CeV/Aula009/js005.js
@@ -0,0 +1,6 @@
+var p1 = document.getElementsByTagName("p")[1]
+document.write(p1.innerHTML)
+
+var d1 = document.getElementById("msg")
+d1.style.color = 'red'
+d1.innerHTML = "compreensível tinha um <strong>bom dia</strong>"
---
+++ CeV/Aula010/js007.js
@@ -0,0 +1,12 @@
+
+
+
+function somar(){
+    var n1 = Number(document.getElementById("txtn1").value)
+    var n2 = Number(document.getElementById("txtn2").value)
+    var res = document.getElementById("res")
+
+    var r = n1 + n2
+
+    res.innerText = r
+}
